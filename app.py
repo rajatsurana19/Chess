419e2fd7aa71c4c42f3093f32e12ae863432cbda
Rajat Surana <rasurana19@gmail.com>
2026-01-17 16:05:26 +0530

    Updated turn step

--- app.py
+++ app.py
@@ -287,7 +287,7 @@ while run:
 
                     black_options = check_options(black_pieces,black_locations,'black')
                     white_options = check_options(white_pieces,white_locations,'white')
-                    turn_step = 2
+                    turn_step = 0
                     selection = 100
                     valid_moves = []
 
