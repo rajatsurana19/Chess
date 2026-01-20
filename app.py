155e345b5e70dabbe3f8e25f4dbb74566e621016
Rajat Surana <rasurana19@gmail.com>
2026-01-20 20:53:46 +0530

    Updated check_knight function

--- app.py
+++ app.py
@@ -198,7 +198,20 @@ def check_bishop(position,color):
 
 #defined check_knight function
 def check_knight(position,color):
-    pass
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        friends_list = black_locations
+        enemies_list = white_locations
+    # 8 squares to check for knights, they can go two squares in one direction and one in another
+    targets = [(1, 2), (1, -2), (2, 1), (2, -1), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
+    for i in range(8):
+        target = (position[0] + targets[i][0], position[1] + targets[i][1])
+        if target not in friends_list and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
+            moves_list.append(target)
+    return moves_list
 
 #def check_queen function
 def check_queen(position,color):
