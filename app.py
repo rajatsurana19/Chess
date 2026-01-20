d5e576b49c18689dfa37368c4d0a107ec4561ac1
Rajat Surana <rasurana19@gmail.com>
2026-01-20 20:50:36 +0530

    Updated check_rook function

--- app.py
+++ app.py
@@ -158,7 +158,39 @@ def check_pawn(position, color):
 
 #defined check_rook function
 def check_rook(position,color):
-    pass
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        enemies_list = white_locations
+        friends_list = black_locations
+    
+    for i in range(4):
+        path = True
+        chain = 1
+        if i == 0:
+            x = 0
+            y = 1
+        elif i == 1:
+            x = 0
+            y = -1
+        elif i == 2:
+            x = 1
+            y = 0
+        else:
+            x = -1
+            y = 0
+        while path:
+            if (position[0] + (chain * x), position[1] + (chain * y)) not in friends_list and \
+                    0 <= position[0] + (chain * x) <= 7 and 0 <= position[1] + (chain * y) <= 7:
+                moves_list.append((position[0] + (chain * x), position[1] + (chain * y)))
+                if (position[0] + (chain * x), position[1] + (chain * y)) in enemies_list:
+                    path = False
+                chain += 1
+            else:
+                path = False
+    return moves_list
 
 #defined check_bishop function
 def check_bishop(position,color):
