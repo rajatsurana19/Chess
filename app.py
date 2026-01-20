8ad8ff0a89c0804d0b3c40e99455601b5b3c4995
Rajat Surana <rasurana19@gmail.com>
2026-01-20 21:37:20 +0530

    Updated check_king function

--- app.py
+++ app.py
@@ -257,7 +257,20 @@ def check_queen(position,color):
 
 #defined check_king function
 def check_king(position,color):
-    pass
+    moves_list = []
+    if color == 'white':
+        enemies_list = black_locations
+        friends_list = white_locations
+    else:
+        friends_list = black_locations
+        enemies_list = white_locations
+    # 8 squares to check for kings, they can go one square any direction
+    targets = [(1, 0), (1, 1), (1, -1), (-1, 0), (-1, 1), (-1, -1), (0, 1), (0, -1)]
+    for i in range(8):
+        target = (position[0] + targets[i][0], position[1] + targets[i][1])
+        if target not in friends_list and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
+            moves_list.append(target)
+    return moves_list
 
 #defined check options
 def check_options(pieces, locations, turn):
